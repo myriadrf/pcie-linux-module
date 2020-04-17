e4d6554c6673468822229fec2a19a7edea649797
Ignas J <ignas.jarusevicius@gmail.com>
2020-04-17 11:24:53 +0300

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-# TODO: use udev instead
 set -e
 make -B
 set +e
-rmmod altera_cvp
-rmmod litepcie
-#rm /dev/litepcie0
-insmod litepcie.ko
 
-#major=$(awk '/ litepcie$/{print $1}' /proc/devices)
-#mknod -m 666 /dev/litepcie0 c $major 0
+if lsmod | grep -Eq "^litepcie"; then
+	rmmod litepcie
+fi
+
+insmod litepcie.ko
 
